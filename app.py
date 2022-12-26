c327ebb5124e386dfb47ec843c95c2c744dde8c1
sysede <103770918+sysede@users.noreply.github.com>
2022-12-25 19:40:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 # --- LOAD CSS, PDF & PROFIL PIC ---
 with open("style.css") as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
-with open(resume_file, "rb") as pdf_file:
+with open("cv.pdf, "rb") as pdf_file:
     PDFbyte = pdf_file.read()
 profile_pic = Image.open(profile_pic)
 
