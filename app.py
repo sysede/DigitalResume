89e0441de5615d2aa804e44a96d2f165b38df1ac
sysede <103770918+sysede@users.noreply.github.com>
2022-12-25 19:41:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -27,14 +27,6 @@ SOCIAL_MEDIA = {
 
 st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 
-
-# --- LOAD CSS, PDF & PROFIL PIC ---
-with open("style.css") as f:
-    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
-with open("cv.pdf") as pdf_file:
-    PDFbyte = pdf_file.read()
-profile_pic = Image.open(profile_pic)
-
 # --- HERO SECTION ---
 col1, col2 = st.columns(2, gap="small")
 with col1:
