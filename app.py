b07f6dec2e344ae277ce1b11ba4434cde477bef8
sysede <103770918+sysede@users.noreply.github.com>
2022-12-25 19:42:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ with open("style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
 # --- PATH SETTINGS ---
-current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
 resume_file = current_dir / "assets" / "CV.pdf"
 profile_pic = current_dir / "assets" / "profile-pic.png"
